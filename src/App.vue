4c96d140d21cb01171bcadfc5b4c121c3d7da309
Nathan Brown <nathan@upventrix.com>
2019-11-24 21:54:47 -0500

    rm duplicate code

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,23 @@ export default {
   },
   data() {
     return {
-      todos: []
+      todos: [
+        {
+          id: 1,
+          title: 'Todo #1',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Todo #2',
+          completed: true,
+        },
+        {
+          id: 3,
+          title: 'Todo #3',
+          completed: false,
+        },
+      ]
     }
   },
   methods: {
